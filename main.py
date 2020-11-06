90c273f76e4045809fda6007fb1b26ea180e7a4d
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-11-06 15:02:30 +0000

    Add some checks to avoid crashing.

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import json
 import requests
 
-# TODO string formatting so I can make a nice looking table of the resuls
-# TODO A loading... message while data is fetched from Universalis
-# TODO maybe use better variable names
-
 universalis_url = "https://universalis.app/api/"
 stored_item_values_address = "btvendoritems.json"
 server = "Cerberus"
@@ -17,15 +13,19 @@ def getItemsFromUniversalis(item_values=None, min_sales=0):
             item_values = stored_item_values
     item_prices = {}
     for i in item_values:
-        with requests.request("GET", universalis_url + server + "/" + str(item_values[i]['id']) + "?entries=1") as response:
-            response_json = response.json()
-            price = response_json['listings'][0]['pricePerUnit']
-            profit = price - item_values[i]['price']
-            #Sales velocity is average sales per day in the past week. Value is capped at 10/7
-            sales = response_json['regularSaleVelocity']*7
-            item_prices[i] = {'price': price,
-                              'profit': profit,
-                              'sales':sales}
+      print("Fetching: " + i)
+      with requests.request("GET", universalis_url + server + "/" + str(item_values[i]['id']) + "?entries=1") as response:
+          if response == None:
+            print(item_values[i]['id'] + " returned None")
+            break
+          response_json = response.json()
+          price = response_json['listings'][0]['pricePerUnit'] if len(response_json['listings']) > 0 else 0
+          profit = price - item_values[i]['price']
+          #Sales velocity is average sales per day in the past week. Value is capped at 10/7
+          sales = response_json['regularSaleVelocity']*7
+          item_prices[i] = {'price': price,
+                            'profit': profit,
+                            'sales':sales}
     sorted_prices = sorted(
         item_prices.items(), key=lambda x: x[1]['profit'], reverse=True)
 
